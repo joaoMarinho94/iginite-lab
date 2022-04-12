c755799dd3596505386e78d874de18ae85c7f25d
joaoMarinho94 <joaopaulim94@gmail.com>
2022-04-12 19:13:53 -0300

    feat: create listAllProducts in purchase

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,7 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 import { DatabaseModule } from 'src/database/database.module';
-import { TestResolver } from './test.resolver';
+import { ProductService } from 'src/services/products.service';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
 
 @Module({
   imports: [
@@ -15,6 +16,6 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [ProductsResolver, ProductService],
 })
 export class HttpModule {}
