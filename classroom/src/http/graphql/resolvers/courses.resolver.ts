9af76cd911ff8ffecfa00c9cf75bc0dd743c211e
joaoMarinho94 <joaopaulim94@gmail.com>
2022-04-13 21:08:32 -0300

    feat: add createCourse to classroom

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,8 +1,9 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/autorization.guard';
 
 import { CoursesService } from 'src/services/courses.service';
+import { CreateCourseInput } from '../inputs/create-course-input';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
@@ -13,5 +14,11 @@ export class CoursesResolver {
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
+  }
+
+  @Mutation(() => Course)
+  @UseGuards(AuthorizationGuard)
+  createCourse(@Args('data') data: CreateCourseInput) {
+    return this.coursesService.createCourse(data);
   }
 }
