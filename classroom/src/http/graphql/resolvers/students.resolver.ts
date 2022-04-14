0c7e5cd40539382d014e50e226ca169e99f5c54b
joaoMarinho94 <joaopaulim94@gmail.com>
2022-04-13 21:01:45 -0300

    feat: create listAllStudents to classroom

---
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -0,0 +1,26 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+
+import { AuthorizationGuard } from 'src/http/auth/autorization.guard';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/students.service';
+import { Student } from '../models/student';
+
+@Resolver(() => Student)
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentService: EnrollmentsService,
+  ) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentService.listEnrollmentByStudentId(student.id);
+  }
+}
